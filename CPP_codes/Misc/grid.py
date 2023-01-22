ec2a77a8a6709d2059c7ddb367b573ddf1144bfb
deep00987 <24670719+deep00987@users.noreply.github.com>
2023-01-22 13:51:21 +0530

    jhatt er assignemnt, life barbad

---
+++ CPP_codes/Misc/grid.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+grid = [[0, 0, 0, 0, 1, 0, 1, 0],
+[0, 1, 0, 1, 0, 1, 0, 1],
+[1, 0, 1, 0, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 0, 1, 0],
+[1, 0, 1, 0, 1, 1, 1, 1],
+[1, 0, 1, 0, 0, 1, 1, 0],
+[0, 1, 1, 1, 1, 0, 1, 0],
+[0, 0, 0, 1, 1, 1, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+def solution(grid, rows, cols):
+    reversed_grid = grid[::-1]
+    max_level = [0]  
+    dirs = [[0, 1, 2], [1, 0, 4], [0, -1, 2], [-1, 0, 1]]
+    cost = [[999 for _ in range(cols)] for _ in range(rows)]
+    visited = set()
+    queue = deque()
+    # print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in reversed_grid]))
+    bfs(0, 0, reversed_grid, rows, cols, dirs, visited, queue, cost, max_level)
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in cost]))
+    print("Max reachable height: ", max_level[0], "Minimum cost: ", min(cost[max_level[0]]))
+
+def bfs(i, j, grid, row, col, dirs, visited, queue, cost, max_level):
+    queue.append((i, j))
+    visited.add((i, j))
+    cost[i][j] = 0
+    while len(queue) > 0:
+        curr = queue.popleft()
+        max_level[0] = max(max_level[0], curr[0])
+        for d in dirs:
+            x1, y1, c1 = curr[0] + d[0], curr[1] + d[1], cost[curr[0]][curr[1]] + d[2]
+            if -1 < x1 < row and -1 < y1 < col:
+                if (x1, y1) not in visited and grid[x1][y1] != 0 and cost[x1][y1] > c1:
+                    queue.append((x1, y1))
+                    cost[x1][y1] = c1
+                    visited.add((x1, y1))
+        
+solution(grid, len(grid), len(grid[0]))
